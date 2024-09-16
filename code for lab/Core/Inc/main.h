d38b24e35fa71539c97a495de949ae1b9cdc9c86
quangminh <lequangminh9768@gmail.com>
2024-09-16 16:17:01 +0700

    no message

--- code for lab/Core/Inc/main.h
+++ code for lab/Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Led0_Pin GPIO_PIN_5
+#define Led0_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
